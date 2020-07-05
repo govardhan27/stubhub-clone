8558b65fa650b5f12ec4d4c622f451c8abb5cd5a
govardhan <27.govardhan@gmail.com>
2020-07-05 15:44:23 +0530

    Added User Model, also got typescript and mangoose to cooperate

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,6 +1,21 @@
 import mongoose from 'mongoose';
 
+// An interface that describes the properties
+// that are required to create new User
 interface UserAttrs {
+	email: string;
+	password: string;
+}
+
+// An interface that describes the properties
+// that User model has
+interface UserModal extends mongoose.Model<UserDoc> {
+	build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User document has
+interface UserDoc extends mongoose.Document {
 	email: string;
 	password: string;
 }
@@ -16,5 +31,8 @@ const userSchema = new mongoose.Schema({
 	},
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
+
+const User = mongoose.model<UserDoc, UserModal>('User', userSchema);
+
 export { User };
